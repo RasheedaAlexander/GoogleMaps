42110403d95e381d1c663952d164dec97f79b3b6
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-08-23 09:50:23 -0400

    Google Maps is displaying all businesses/gems.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
-(function() {
-  angular.module('hiddngem', []);
-})()
+//Angular App Module and Controller
+          var sampleApp = angular.module('mapsApp', []);
+          sampleApp.controller('MapCtrl', function ($scope) {
+
+              var mapOptions = {
+                  zoom: 4,
+                  center: new google.maps.LatLng(25,80),
+                  mapTypeId: google.maps.MapTypeId.TERRAIN
+              }
+
+              $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+              $scope.markers = [];
+
+              var infoWindow = new google.maps.InfoWindow();
+
+              var createMarker = function (info){
+
+                  var marker = new google.maps.Marker({
+                      map: $scope.map,
+                      position: new google.maps.LatLng(info.lat, info.lng),
+                      title: info.name
+                  });
+                  marker.content = '<div class="infoWindowContent">' + info.description + '</div>';
+
+                  google.maps.event.addListener(marker, 'click', function(){
+                      infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                      infoWindow.open($scope.map, marker);
+                  });
+
+                  $scope.markers.push(marker);
+
+              }
+
+              for (i = 0; i < data.length; i++){
+                  createMarker(data[i]);
+              }
+
+              $scope.openInfoWindow = function(e, selectedMarker){
+                  e.preventDefault();
+                  google.maps.event.trigger(selectedMarker, 'click');
+              }
+
+          });
